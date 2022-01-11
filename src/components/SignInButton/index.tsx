e0f73ab29ab6a3aac7d7e572925d9ea9ad8c4d3e
Agustinho Neto <agustinho.pneto@gmail.com>
2022-01-11 11:22:22 -0300

    feat(Header): add SignIn Button

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,25 @@
+import styles from './styles.module.scss';
+
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return (
+    <button 
+      type="button" 
+      className={styles.signInButton}
+    >
+      <FaGithub color={isUserLoggedIn ? "#04D361" : "#EBA417"} />
+      {
+        isUserLoggedIn ? (
+          <> 
+            Agustinho Neto 
+            <FiX color="#737380" className={styles.closeIcon} />
+          </>
+        ) : "Sign in with GitHub"
+      }
+    </button>
+  )
+}
